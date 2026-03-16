c29f958272bb43964f15c34c594d53d6c0fa22cc
Atul-Raj7 <meatulraj7@gmail.com>
2026-03-16 17:40:41 +0530

    read function

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const fs = require("fs")
 const path = require("path")
 
+const filePath = path.join(process.cwd(), "task.json")
+
+//read
+function readTask () {
+  if(fs.existsSync(filePath)){
+    const data = fs.readFileSync(filePath, "utf-8")
+    return JSON.parse(data)
+  }
+  return []
+}
+
+//add
+
+//update
+//delete
+//mark of progress and done
+// list all task
+// list that are done
+// list that are in progress
